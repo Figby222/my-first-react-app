86f2d97dc4d8f36febe75cbe46ac390a6fc28118
Figby222 <figby222@outlook.com>
2024-04-29 20:28:18 -0700

    Add and display Greeting

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import HelloWorld from './Hello-World.jsx';
+import Greeting from './components/Greeting.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelloWorld />
+    <Greeting />
   </React.StrictMode>,
 )
